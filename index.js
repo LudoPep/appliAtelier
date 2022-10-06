767d6a047a0901975bac7f964ba9536c6d6deb46
Ludo PEPIN <ludo.pepin@yahoo.com>
2022-10-06 09:59:56 +0200

    index.js

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ if(PORT == null || PORT == "") {
 express()
   .set('html', path.join(__dirname, 'src'))
   // .set('view engine', 'ejs')
-  .get('/', (req, res) => res.sendFile('/app/root/home/body/body.component.html'))
+  .get('/', (req, res) => res.sendFile('/src/app/root/home/body/body.component.html'))
   .listen(PORT)
 
 
   router.get('/Home',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/root/home/body/body.component.html'));
+    res.sendFile(path.join(__dirname+'/src/app/root/home/body/body.component.html'));
   });
   
   router.get('/Details',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/root/detail/card/card.component.html'));
+    res.sendFile(path.join(__dirname+'/src/app/root/detail/card/card.component.html'));
   });
 
   app.use('/', router);
